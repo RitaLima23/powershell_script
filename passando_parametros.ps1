71f2c218d075296bf545df96d2bbf2beae68401d
Rita Lima <45838303+RitaLima23@users.noreply.github.com>
2021-06-07 22:43:06 -0300

    Efetua ping no host informado

---
+++ passando_parametros.ps1
@@ -0,0 +1,8 @@
+﻿param ($ip)
+if ($ip){
+echo "Rita Lima - Passando parametros"
+echo "Modo de uso: .\passando_parametros 192.168.0.1"
+}else{
+echo "Efetuando ping no Host: $ip"
+ping -n 1 $ip | Select-String "bytes-32"
+}

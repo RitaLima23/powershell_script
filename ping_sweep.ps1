76b13df876a15bd4c2ecc2041a26475023b5db94
Rita Lima <45838303+RitaLima23@users.noreply.github.com>
2021-06-07 22:55:16 -0300

    passa ping para uma nova variável
     e simplifica a saída

---
+++ ping_sweep.ps1
@@ -0,0 +1,12 @@
+﻿#RITA LIMA - PING SWEEP
+param ($p1)
+if(!$p1){
+echo "RITA LIMA - PROGRAM"
+echo "Exemplo de uso: .\ping_sweep.ps1 192.168.0"
+}else{
+foreach ($ip in 1...254){
+try{$resp = ping -n 1 "$p1.$ip" | Select-String "bytes=32"
+$resp.Line.Split(' ')[2] -replace ":", " "
+}catch {}
+ }
+  } 

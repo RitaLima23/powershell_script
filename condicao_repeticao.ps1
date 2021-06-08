058d2bb2d0c9f6e534ac7ebe2267a5e7c490eddd
Rita Lima <45838303+RitaLima23@users.noreply.github.com>
2021-06-07 22:48:21 -0300

    Condição/Repetição
    Realiza a verificação do ip entre 1..254

---
+++ condicao_repeticao.ps1
@@ -0,0 +1,9 @@
+﻿param ($p1)
+if (!$p1){
+echo "Rita Lima - Program"
+echo "Exemplo de uso: .\script2.ps1 192.168.0"
+}else{
+foreach ($ip in 1..254){
+ping -n 1 "$p1.$ip" | Select-String "bytes=32"
+}
+}
